13396ae23aa1e37cf4dbf092617db30276909527
kirillF <kirill.filimonov@gmail.com>
2015-04-05 03:48:43 +0300

    init

--- app/src/main/java/io/github/kirillf/hashviewer/Constants.java
+++ app/src/main/java/io/github/kirillf/hashviewer/Constants.java
@@ -1,8 +1,8 @@
 package io.github.kirillf.hashviewer;
 
 public class Constants {
-    public static final String consumerKey = "mFwJEbGKBgJ1bsrt90A5TqSJL";
-    public static final String consumerSecret = "BIrOMwd3nrv2vdb7kAK5O3lJyDIH6LEr8wiAc1dKDgmWsSm4eS";
+    public static final String consumerKey = "--- YOUR KEY ---";
+    public static final String consumerSecret = "--- YOUR SECRET ---";
 
     public static final String TWITTER_OAUTH_URL = "https://api.twitter.com/oauth2/token";
     public static final String TWITTER_SEARCH_API = "https://api.twitter.com/1.1/search/tweets.json";
